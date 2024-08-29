7dbf0bebe69ff4fcfaf3f4598430b993a5d2e4ee
brryan12 <bryancr1208@gmail.com>
2024-08-29 15:09:28 -0600

    contenedora mascatas y dueño avance

--- ContenedoraMascotas.cpp
+++ ContenedoraMascotas.cpp
@@ -1,1 +1,27 @@
 #include "ContenedoraMascotas.h"
+
+ContenedoraMascotas::ContenedoraMascotas(int tam)
+{
+	this->tam = tam;
+	this->cantidad = 0;
+	this->mascotas = new Mascota * [tam];
+	for (int i = 0; i < tam; i++) {
+		mascotas[i] = nullptr;
+	}
+}
+
+ContenedoraMascotas::~ContenedoraMascotas()
+{
+	for (int i = 0; i < cantidad; i++) {
+		delete mascotas[i];
+	}
+	delete[] mascotas;
+}
+
+void ContenedoraMascotas::agregarMascota(Mascota* mascota)
+{
+	if (cantidad < tam) {
+		mascotas[cantidad] = mascota;
+		cantidad++;
+	}
+}
--- Dueño.h
+++ Dueño.h
@@ -1,10 +1,23 @@
 #pragma once
 #include <iostream>
+#include "ContenedoraMascotas.h"
 using namespace std;
 class Dueño
 {
 private:
 	string nombre;
 	string cedula;
+	ContenedoraMascotas mascotas;
+public:
+	Dueño(string nombre, string cedula);
+	~Dueño();
+	string getNombre();
+	string getCedula();
+	void setNombre(string nombre);
+	void setCedula(string cedula);
+	void agregarMascota(Mascota* mascota);
+	Mascota* buscarMascota(string nombre);
+	void eliminarMascota(string nombre);
+	void mostrarMascotas();
 };
 

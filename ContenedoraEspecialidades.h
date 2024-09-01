f301bf704613fdd2dd052bb0fed7de55e8e93a7d
filipo2311 <jeanfilipo23@gmail.com>
2024-09-01 16:38:20 -0600

    Le meti algo al Contenedor de Mascotas, aunque hay que agregar las 8 Especialidades

--- ContenedoraEspecialidades.h
+++ ContenedoraEspecialidades.h
@@ -1,5 +1,38 @@
 #pragma once
+#include "Especialidad.h"
+#include <iostream>
+#include <sstream>
+using namespace std;
+
 class ContenedoraEspecialidades
 {
-};
+private:
+	Especialidad** Especial;
+	int Cant;
+	int tam;
+public:
+	ContenedoraEspecialidades(int tam) {
+		Especial = new Especialidad*[tam];
+		this->tam + tam;
+	}
 
+	virtual ~ContenedoraEspecialidades() {
+		for (int i = 0; i < Cant; i++) {
+			delete Especial[i];
+		}
+		delete[] Especial;
+		cout << "Contenedor destruido" << endl;
+	}
+
+	void agregarEspecialidad(Especialidad* nuevaEspecialidad) {
+		if (Cant < tam) {
+			Especial[Cant] = nuevaEspecialidad;
+			Cant++;
+		}
+		else {
+			cout << "No se pueden agregar más especialidades, contenedor lleno." << endl;
+		}
+	}
+
+	string toString();
+};

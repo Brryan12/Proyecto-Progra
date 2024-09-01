3ac38f911c531df75e0bc56ff160d443c7d8b251
brryan12 <bryan.aguero.mata@est.una.ac.cr>
2024-09-01 00:25:20 -0600

    Creacion de algunos metodos en la contenedora de dueño

--- ContenedoraDueño.h
+++ ContenedoraDueño.h
@@ -1,6 +1,16 @@
 #pragma once
+#include "Dueño.h"
 class ContenedoraDueño
 {
-	
+private:
+	Dueño** dueños;
+	int cant;
+	int tam;
+public:
+	ContenedoraDueño(int tam);
+	~ContenedoraDueño();
+	bool existeDueño(string cedula);
+	void agregarDueño(Dueño* dueño);
+	string toString();
 };
 

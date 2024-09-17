536d65c1729e0180e2a492a3ab67d1200155a01c
brryan12 <bryan.aguero.mata@est.una.ac.cr>
2024-09-17 13:48:19 -0600

    igual

--- Dueno.h
+++ Dueno.h
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-
+#include "ContenedoraMascotas.h"
+#include "Mascota.h"
+#include "ContenedoraCitas.h"
 using namespace std;
 
 class Dueno
